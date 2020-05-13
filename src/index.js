bc504c9d1e3894fb53b4a454c2415f521e5ce2db
Diego Guevara <diegog.guevarab@gmail.com>
2020-05-13 00:42:31 -0500

    componente PrivateRoute y conexion a Redux Store

--- src/index.js
+++ src/index.js
@@ -1,43 +1,75 @@
-/*!
-
-=========================================================
-* Material Dashboard PRO React - v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
+import { Provider, connect } from "react-redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
-
-import AuthLayout from "layouts/Auth.js";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import AdminLayout from "layouts/Admin.js";
-
+import { auth } from "actions";
+import frutiApp from "reducers";
 import "assets/scss/material-dashboard-pro-react.scss?v=1.8.0";
 import LandingPage from "views/LandingPage/LandingPage";
 import LoginPage from "views/Pages/LoginPage";
 import RegisterPage from "views/Pages/RegisterPage";
+import ErrorPage from "./views/Pages/ErrorPage";
 
 const hist = createBrowserHistory();
+let store = createStore(frutiApp, applyMiddleware(thunk));
+
+const PrivateRoute = ({ Component: ChildComponent, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      this.props.auth.isAuthenticated ? (
+        <ChildComponent {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
+
+class RootContainerComponent extends React.Component {
+  componentDidMount() {
+    this.props.loadUser();
+  }
+  render() {
+    return (
+      <Router history={hist}>
+        <Switch>
+          <PrivateRoute path="/admin" component={AdminLayout} />
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/register" exact component={RegisterPage} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadUser: () => {
+      return dispatch(auth.loadUser());
+    }
+  };
+};
+let RootContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RootContainerComponent);
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/auth" component={AuthLayout} />
-      <Route path="/admin" component={AdminLayout} />
-      <Route path="/" exact component={LandingPage} />
-      <Route path="/login" exact component={LoginPage} />
-      <Route path="/register" exact component={RegisterPage} />
-    </Switch>
-  </Router>,
+  <Provider store={store}>
+    <RootContainer />
+  </Provider>,
   document.getElementById("root")
 );
