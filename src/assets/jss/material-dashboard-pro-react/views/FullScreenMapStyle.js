5baa8158437e89cc6579155dbaa9bfa61a36ac2d
Diego Guevara <diegog.guevarab@gmail.com>
2020-05-16 23:15:05 -0500

    vista de mapa terminada

---
+++ src/assets/jss/material-dashboard-pro-react/views/FullScreenMapStyle.js
@@ -0,0 +1,12 @@
+const fullscreenMapStyle = {
+  App: {
+    textAlign: "center",
+    height: "100vh"
+  },
+
+  AppMap: {
+    height: "100%",
+    width: "100%"
+  }
+};
+export default fullscreenMapStyle
--- src/views/Maps/FullScreenMap.js
+++ src/views/Maps/FullScreenMap.js
@@ -1,36 +1,118 @@
 import React from "react";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
+import { connect } from "react-redux";
+import { parse } from "wkt";
+import { LoadScript, GoogleMap, Polygon, Marker } from "@react-google-maps/api";
+import { makeStyles } from "@material-ui/core/styles";
+import { farms, trees } from "actions";
 
-const CustomSkinMap = withScriptjs(
-  withGoogleMap(() => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 4.6493355330066, lng: -74.395033434259 }}
-      defaultOptions={{
-        scrollwheel: false,
-        zoomControl: true,
-        mapTypeId: "hybrid",
-        mapTypeControl: false,
-        streetViewControl: false
-      }}
-    >
-      <Marker position={{ lat: 4.6493355330066, lng: -74.395033434259 }} />
-    </GoogleMap>
-  ))
-);
+import styles from "assets/jss/material-dashboard-pro-react/views/FullScreenMapStyle";
 
-export default function FullScreenMap() {
+const useStyles = makeStyles(styles);
+
+function FullScreenMap(props) {
+  const classes = useStyles();
+  const [farms, setFarms] = React.useState([]);
+  const [trees, setTrees] = React.useState([]);
+  const [center, setCenter] = React.useState({
+    lat: 4.644048217838231,
+    lng: -74.39119476824999
+  });
+  const mapFarms = myFarms => {
+    return myFarms.map((farm, key) => {
+      let farm_WKT = farm.polygon.substring(farm.polygon.indexOf(";") + 1);
+      let farm_points = parse(farm_WKT).coordinates[0];
+      let farm_path = farm_points.map(point => {
+        return { lat: point[0], lng: point[1] };
+      });
+      return [<Polygon path={farm_path} key={key} />];
+    });
+  };
+  const mapTrees = myTrees => {
+    return myTrees.map((tree, key) => {
+      let tree_WKT = tree.location.substring(tree.location.indexOf(";") + 1);
+      let tree_point = parse(tree_WKT).coordinates;
+      let tree_position = { lat: tree_point[0], lng: tree_point[1] };
+      return (
+        <Marker
+          position={tree_position}
+          key={key}
+          title={`Árbol: ${tree.id}. ${getSpecie(tree.specie)}`}
+        />
+      );
+    });
+  };
+  React.useEffect(() => {
+    props.fetchFarms();
+    props.fetchTrees();
+  }, []);
+  React.useEffect(() => {
+    setFarms(mapFarms(props.farms));
+    setTrees(mapTrees(props.trees));
+  }, [props.farms, props.trees]);
   return (
-    <CustomSkinMap
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDyXNgKS0gtLrB611nCh-J3gVqu7v_via0"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `100vh` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
+    <div className={classes.App}>
+      <LoadScript
+        id="script-loader"
+        googleMapsApiKey="AIzaSyDyXNgKS0gtLrB611nCh-J3gVqu7v_via0"
+        language="es"
+        region="co"
+      >
+        <GoogleMap
+          mapContainerClassName={classes.AppMap}
+          center={center}
+          zoom={20}
+          options={{
+            scrollwheel: false,
+            zoomControl: true,
+            mapTypeId: "hybrid",
+            mapTypeControl: false,
+            streetViewControl: false
+          }}
+          version="weekly"
+          on
+        >
+          {farms}
+          {trees}
+        </GoogleMap>
+      </LoadScript>
+    </div>
   );
 }
+const getSpecie = specie => {
+  switch (specie) {
+    case "M":
+      return "Mango Tommy";
+    case "F":
+      return "Mango Farchild";
+    case "N":
+      return "Naranja";
+    case "A":
+      return "Aguacate";
+    case "D":
+      return "Mandarina";
+    case "L":
+      return "Limón";
+    case "B":
+      return "Banano";
+    default:
+      return "Frutal";
+  }
+};
+const mapStateToProps = state => {
+  return {
+    farms: state.farms,
+    trees: state.trees
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchFarms: () => dispatch(farms.fetchFarms()),
+    fetchTrees: () => dispatch(trees.fetchTrees())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FullScreenMap);
